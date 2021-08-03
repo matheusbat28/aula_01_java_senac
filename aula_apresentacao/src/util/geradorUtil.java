8bc4ed2979b9c70f7873f78608e5e1763f2edf98
matheus.batista1 <matheus.batista1@PLE111D03.alunos.sc.senac.br>
2021-08-02 21:59:06 -0300

    gerador numero aleatorio e a gerador cpf

--- aula_apresentacao/src/util/geradorUtil.java
+++ aula_apresentacao/src/util/geradorUtil.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,15 +11,30 @@ package util;
  * @author matheus.batista1
  */
 public class geradorUtil {
-    
-    public double gerarSalario(){
-        int salaroAleatorio = (int) (Math.random() * 100000); 
+
+    public double gerarSalario() {
+        int salaroAleatorio = (int) (Math.random() * 100000);
         return salaroAleatorio;
     }
-    
+
+    public String gerarNumero(int quantidade) {
+        String senha = "";
+        int numero;
+        for (int i = 0; i < quantidade; i++) {
+            numero = (int) (Math.random() * 10);
+            senha += numero;
+        }
+        return senha;
+    }
+    public  String formatarCpf(){
+         return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + 
+                 "-" + gerarNumero(2);
+    }
+
+    //public String gerarCpfAleatorio() {
+    //}    
     public static void main(String[] args) {
-       
-        
-        System.out.println();
+        geradorUtil util = new geradorUtil();
+        System.out.println(util.formatarCpf());
     }
 }
