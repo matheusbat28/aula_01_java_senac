b06696b6e9c6bc9f1d28c915b79cfe726442342f
matheus.batista1 <matheus.batista1@PLE111D03.alunos.sc.senac.br>
2021-08-03 21:50:22 -0300

    gerar nome completo

--- aula_apresentacao/src/util/geradorUtil.java
+++ aula_apresentacao/src/util/geradorUtil.java
@@ -26,15 +26,50 @@ public class geradorUtil {
         }
         return senha;
     }
+    public String gerarNomeCompleto(){
+        String[] nomes = {"joão", "maria", "matheus", "nataly", "silvio", 
+            "pedro", "lucas", "josé", "heloisa", "marisa", "adam", "tania", 
+            "cassia", "antonia", "antony", "samara", "hugo", "leticia", "suelen",
+        "ana", "osvaldo"};
+        int indice = (int) (Math.random()* nomes.length);
+        return nomes[indice] + " " + gerarSobrenomes();
+    }
+    private String gerarSobrenomes(){
+        String[] sobrenomes = {"silva", "batista", "butka", "carvalho", "pereira", 
+            "peixoto"};
+        int indice = (int) (Math.random()* sobrenomes.length);
+        return sobrenomes[indice];
+    }
     public  String formatarCpf(){
          return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + 
                  "-" + gerarNumero(2);
     }
+    public String gerarCnpj(){
+        return gerarNumero(2) + "." + gerarNumero(3) + "." + gerarNumero(3)+ "/"
+                + gerarNumero(4) + "-" + gerarNumero(2);
+    }
+    public String gerarTfFixo(){
+        return "(48)3"+ gerarNumero(3) + "-" + gerarNumero(4);
+    }
+    public String gerarTfCelular(){
+        return "(48)9"+ gerarNumero(4) + "-" + gerarNumero(4);
+    }
+    public String gerarCep(){
+        return gerarNumero(5) + "-" + gerarNumero(3);
+    }
+
+    
 
     //public String gerarCpfAleatorio() {
     //}    
-    public static void main(String[] args) {
-        geradorUtil util = new geradorUtil();
-        System.out.println(util.formatarCpf());
+    public static void main(String[] args){
+        geradorUtil teste = new geradorUtil();
+        
+        System.out.println(teste.formatarCpf());
+        System.out.println(teste.gerarCnpj());
+        System.out.println(teste.gerarTfFixo());
+        System.out.println(teste.gerarTfCelular());
+        System.out.println(teste.gerarCep());
+        System.out.println(teste.gerarNomeCompleto());
     }
 }
